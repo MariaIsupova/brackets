fc8bfe4409ec7a46dac79075ebdc72c2fe5b38c4
MariaIsupova <Mariaisisupova@gmail.com>
2020-10-07 22:45:23 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
 module.exports = function check(s, bracketsConfig) {
-    const brackets = {
-        ')': '(',
-        ']': '[',
-        '}': '{'
-    };
+    let brackets = {}
+    s = s.split('')
+    bracketsConfig.forEach(element => {
+        if (element[0] === element[1]) {
+            let f = true
+            for (k = 0; k < s.length; k++) {
+                if (s[k] === element[0]) {
+                    if (f) f = false
+                    else {
+                        s[k] += '*'
+                        f = true
+                    }
+                }
+            }
+            brackets[element[1] + '*'] = element[0]
+        } else {
+            brackets[element[1]] = element[0]
+        }
+    });
+
     let flag = true
     const stack = [];
     for (let i = 0; i < s.length; i++) {
-        console.log('s_i',s[i])
-        if (s[i] === '|') {
-            if (flag) {
-                stack.push(s[i]);
-                flag = false
-                // console.log('push stack', stack)
-            } else {
-                // console.log('stack ', stack, [s[i]])
-                if (s[i] !== stack.pop()) {
-                    return false;
-                }
-                flag=true
+        if (isClosedBracket(s[i])) {
+            // console.log('stack ', stack, brackets[s[i]])
+            if (brackets[s[i]] !== stack.pop()) {
+                return false;
             }
-        } 
-        else {
-            if (isClosedBracket(s[i])) {
-                console.log('stack ', stack, brackets[s[i]])
-                if (brackets[s[i]] !== stack.pop()) {
-                    return false;
-                }
-            } else {
-                stack.push(s[i]);
-                console.log('push stack', stack)
-            }
+        } else {
+            stack.push(s[i]);
+            // console.log('push stack', stack)
         }
     }
     return stack.length === 0
 }
 
 function isClosedBracket(ch) {
-    return [')', ']', '}','2','4','6'].indexOf(ch) > -1;
+    return [')', ']', '}', '2','4','6','|*', '8*', '7*'].indexOf(ch) > -1;
 }
